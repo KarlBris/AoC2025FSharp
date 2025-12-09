e2f0af19ee8a0d8e78ec5b65d565dd3dd70ec469
Karl Bristav <karl.bristav@antura.se>
2025-12-09 19:58:19 +0100

    wtf

--- AoC2025/Day09.fs
+++ AoC2025/Day09.fs
@@ -20,114 +20,142 @@ module Day09 =
         |> List.head
         |> string
 
-    let rec fourWise (elements: 'a list) : ('a * 'a * 'a * 'a) list =
-        match elements with
-        | a :: b :: c :: d :: rest -> (a, b, c, d) :: (fourWise (b :: c :: d :: rest))
-        | _ -> []
-
-    let isSaddleOrVertical (((aX, aY), (bX, bY), (cX, cY), (dX, dY)): Position * Position * Position * Position) : bool =
-        if bY = cY then
-            (aY < bY && dY > bY) || (aY > bY && dY < bY)
-        else
-            true
-
-    let rec isPointOnALine (lines: (Position * Position) list) (point as (pX, pY): Position) : (Position * Position) option =
+    let rec isPointOnALine (lines: (Position * Position) list) (point as (pX, pY): Position) : bool =
         match lines with
-        | [] -> None
+        | [] -> false
         | ((aX, aY), (bX, bY)) :: ls ->
             let isOnThisLine =
-                if pX = aX then pY >= (min aY bY) && pY <= (max aY bY)
-                elif pY = aY then pX >= (min aX bX) && pX <= (max aX bX)
-                else false
+                pX >= (min aX bX) && pX <= (max aX bX) && pY >= (min aY bY) && pY <= (max aY bY)
+                // if pX = aX then pY >= (min aY bY) && pY <= (max aY bY)
+                // elif pY = aY then pX >= (min aX bX) && pX <= (max aX bX)
+                // else false
 
-            if isOnThisLine then Some ((aX, aY), (bX, bY)) else isPointOnALine ls point
+            if isOnThisLine then true else isPointOnALine ls point
 
-    let rec lineIntersections (start : int) (lines: (Position * Position) list) (point as (pX, pY): Position) : int =
-        if start >= pX
-            then 0
-            else
-                let onLine = isPointOnALine lines (start,pY)
-                match onLine with
-                | None -> lineIntersections (start+1) lines point
-                | Some ((aX, _), (bX, _)) ->
-                    let lineWidth =
-                        if aX = bX then 1
-                        else abs (aX-bX)
-                    1 + lineIntersections (start + lineWidth+1) lines point
+    let crossProduct ((aX, aY): Position, (bX, bY): Position, (cX, cY): Position) : int =
+        ((bX - aX) * (cY - aY)) - ((bY - aY) * (cX - aX))
     
-    let shortenLineIfVertical (line as ((aX, aY), (bX, bY)):(Position * Position)) : (Position * Position) option =
-        if aY = bY then
-           Some line
-        else
-            if aX < bX then // aX above
-                let aY' = aY + 1
-                let bY' = bY - 1
-                if aY' > bY' then None else Some ((aX, aY'), (bX, bY'))
-            else // bX above
-                let aY' = aY - 1
-                let bY' = bY + 1
-                if aY' < bY' then None else Some ((aX, aY'), (bX, bY'))
-
-    let isRectangleLegal (positions: Position array) (allLines: (Position * Position) list) (shortenedLines: (Position * Position) list) ((a,b) as ((aX, aY): Position, (bX, bY): Position)) : bool =
+    let isPointInsidePolygon (lines: (Position * Position) list) ((pX,pY): Position) : bool =
+        let windingNumber =
+            lines
+            |> List.fold (fun wn ((aX,aY),(bX,bY)) ->
+            let cross = -crossProduct ((aX,aY),(bX,bY),(pX,pY))
+            
+            if aY <= pY then
+                if aY > pY && cross > 0 then (wn + 1) else wn
+            else
+                if bY <= pY && cross < 0 then (wn - 1) else wn
+            ) 0
+        
+        windingNumber <> 0
+    
+    // let isPointInsidePolygonRev (lines: (Position * Position) list) ((pX,pY): Position) : bool =
+    //     let windingNumber =
+    //         lines
+    //         |> List.fold (fun wn ((aX,aY),(bX,bY)) ->
+    //         let cross = crossProduct ((aX,aY),(bX,bY),(pX,pY))
+    //         
+    //         if aY <= pY then
+    //             if aY > pY && cross > 0 then (wn + 1) else wn
+    //         else
+    //             if bY <= pY && cross < 0 then (wn - 1) else wn
+    //         ) 0
+    //     
+    //     windingNumber <> 0
+    
+    let rec arePositionsInside(allLines: (Position * Position) list) (positions: Position list) :  bool=
+        match positions with
+        | [] ->true
+        | pos::ps ->
+                let isOnLine = isPointOnALine allLines pos
+                //let isOnLine = false
+                if isOnLine then
+                    arePositionsInside allLines ps
+                else
+                    let isInside = isPointInsidePolygon allLines pos
+                    if isInside then
+                        arePositionsInside allLines ps else false
+        
+    let isRectangleLegal (allLines: (Position * Position) list)((a,b) as ((aX, aY): Position, (bX, bY): Position)) :bool =
         let (c,d) = (aX, bY), (bX, aY)
         
-        printfn $"Rect {(a,b)}"
+        let topLeft = (min aX bX, min aY bY)
+        let topRight = (max aX bX, min aY bY)
+        let bottomLeft = (min aX bX, max aY bY)
+        let bottomRight = (max aX bX, max aY bY)
         
-        if aX = 9 && aY = 5 && bX = 2 && bY = 3 then
-            ()
-         
-        let outline = [(makeLineBetween a c); (makeLineBetween c b); (makeLineBetween b d); (makeLineBetween d a); ] |> List.concat
+        let topLeftDirs = manhattanNeighborPositions |> Array.map (fun (p,d) -> addPos topLeft p, d) |> Array.filter (fun (_,d) -> d = E || d = S) |> Array.map fst
+        let topRightDirs = manhattanNeighborPositions |> Array.map (fun (p,d) -> addPos topRight p, d) |> Array.filter (fun (_,d) -> d = W || d = S) |> Array.map fst
+        let bottomLeftDirs = manhattanNeighborPositions |> Array.map (fun (p,d) -> addPos bottomLeft p, d) |> Array.filter (fun (_,d) -> d = E || d = N) |> Array.map fst
+        let bottomRightDirs = manhattanNeighborPositions |> Array.map (fun (p,d) -> addPos bottomRight p, d) |> Array.filter (fun (_,d) -> d = W || d = N) |> Array.map fst
+        let isPrelimValid = [| topLeftDirs; topRightDirs; bottomLeftDirs; bottomRightDirs |] |> Array.concat |> List.ofArray |> arePositionsInside allLines
+        if not isPrelimValid then false
+            else 
         
-        let areAllOutlinePositionsInside =
-            outline
-            |> List.map (fun pos ->
-                if fst pos = 2 && snd pos = 7 then
-                    ()
-                let isOnLine = isPointOnALine allLines pos |> Option.isSome
-                if isOnLine then true
-                    else
-                        let intersections = lineIntersections 0 shortenedLines pos
-                        (intersections % 2) = 1
-                )
-            |> List.forall (fun b -> b = true)
-        printfn $"areAllOutlinePositionsInside {areAllOutlinePositionsInside}"
-        
-        // let isOneProspectiveCornerInTheList =
-        //     (prospectiveCorners
-        //      |> Array.filter (fun pP -> Array.contains pP positions)
-        //      |> Array.length) > 1
-
-        let isAnyPositionInsideTheRectangle =
-            positions
-            |> Array.map (fun (pX, pY) -> pX > (min aX bX) && pX < (max aX bX) && pY > (min aY bY) && pY < (max aY bY))
-            |> Array.contains true
-
-        printfn $"isAnyPositionInsideTheRectangle {isAnyPositionInsideTheRectangle}"
-        areAllOutlinePositionsInside && (not isAnyPositionInsideTheRectangle)
+                let area = area (a,b)
+                
+                let timer = System.Diagnostics.Stopwatch.StartNew()
+                
+                
+                
+                let outline = [(makeLineBetween a c); (makeLineBetween c b); (makeLineBetween b d); (makeLineBetween d a); ] |> List.concat
+                let areAllOutlinePositionsInside = arePositionsInside allLines outline
+                
+                
+                
+                timer.Stop()
+                // printfn $"Rect {(a,b)}"
+                // printfn $"memoMap: {memoMap.Count}"
+                // printfn $"area: {area}"
+                // printfn $"outline: {outline.Length}"
+                // printfn $"{timer.Elapsed}"
+                areAllOutlinePositionsInside
                 
     let part2 (input: string) : string =
         let positions = input |> parseInput
+        let revPositions = input |> parseInput |> Array.rev
 
-        let lastTwoPositions = positions |> Array.rev |> Array.take 2 |> Array.rev
-        let firstPosition = Array.head positions
-
+        let crossTest1 = crossProduct ((0,0),(5,0),(3,1))
+        let crossTest2 = crossProduct ((5,0),(0,0),(3,1))
+        
         let allLines = [|[|Array.last positions|];positions|] |> Array.concat |> Array.pairwise |> List.ofArray
+        let revAllLines = [|[|Array.last revPositions|];revPositions|] |> Array.concat |> Array.pairwise |> List.ofArray
         
-        let saddleLines =
-            [| lastTwoPositions; positions; [| firstPosition |] |]
-            |> Array.concat
-            |> List.ofArray
-            |> fourWise
-            |> List.choose (fun (a, b, c, d) -> if isSaddleOrVertical (a, b, c, d) then Some(b, c) else None)
-
-        let shortenedLines = saddleLines |> List.choose shortenLineIfVertical
+        let allPointsInSpace =
+            [ for a in 0 .. 13 do
+                for b in 0 .. 8 do
+                 yield (a, b) ]
         
-        positions
-        |> List.ofArray
-        |> makePairs
-        |> List.filter (isRectangleLegal positions allLines shortenedLines)
-        |> List.map (fun ps -> area ps, ps)
-        |> List.sortByDescending fst
-        |> List.map fst
-        |> List.head
-        |> string
+        let allPositions  = allLines |> List.map (fun (a,b) -> makeLineBetween a b) |> List.concat
+        
+        let drawMap = allPositions |> List.map (fun p -> p, '#') |> Map.ofList
+        printMap (19,8) drawMap
+        printfn "\n"
+        let insideMap = allPointsInSpace |> List.filter
+                                                (fun p ->
+            let p' = addPos (-1,0) p
+            isPointInsidePolygon allLines p'
+            //|| isPointOnALine allLines p
+                        )
+                        |> List.map (fun p -> p, 'o') |> Map.ofList
+        printMap (19,8) insideMap
+        
+        "12"
+        
+        // let pairs =
+        //     positions
+        //     |> List.ofArray
+        //     |> makePairs
+        // printfn $"{pairs.Length}"
+        //
+        // pairs
+        // |> List.fold (fun (legalRects, counter) rect ->
+        //     if counter % 100 = 0 then printfn $"{counter}/{pairs.Length}"
+        //     let (isLegal) = isRectangleLegal allLines rect
+        //     if isLegal then (rect::legalRects, counter+1) else (legalRects, counter+1)) ([], 0)
+        // |> fst
+        // |> List.map (fun ps -> area ps, ps)
+        // |> List.sortByDescending fst
+        // |> List.map fst
+        // |> List.head
+        // |> string
